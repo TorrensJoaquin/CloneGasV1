e221dd6f2c24aa7c19ca21c85ebd8b0c5623a16d
Joaquín Torrens <71098233+TorrensJoaquin@users.noreply.github.com>
2022-07-09 19:18:44 -0300

    Update AUTREADINGBOX.js

--- AUTREADINGBOX.js
+++ AUTREADINGBOX.js
@@ -44,6 +44,8 @@ function generateTable(data) {
         TransposeTheDataArray();
     }
     for(let i=0; i < NumberOfRows; i++){
+        data[i][1] = data[i][1].replace(',', '.');
+
         data[i][0] = data[i][0].toUpperCase();
         data[i][0] = data[i][0].replace('PC', '');
         data[i][0] = data[i][0].replace('_', '');
